23d82658aa40a307f345c04fd76c7a38e9b760e8
codrinursachi <codrinursachi@gmail.com>
2024-02-17 09:36:34 +0200

    Formatting

--- Lab1Ex5/Program.cs
+++ Lab1Ex5/Program.cs
@@ -5,5 +5,5 @@ Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cel
 doua valori in ordine crescatoare.
 
 • Exemplu: citim 9,0 Afisam : 0 9*/
-const uint numarElemente = 2;
-ShowNumbers(SortNumbers(ReadNumbers(numarElemente)));
+const uint NUMAR_ELEMENTE = 2;
+ShowNumbers(SortNumbers(ReadNumbers(NUMAR_ELEMENTE)));
