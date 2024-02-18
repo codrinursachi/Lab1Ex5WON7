15779249a57c204208774700343a0c0e85979a5f
codrinursachi <codrinursachi@gmail.com>
2024-02-18 08:32:36 +0200

    Formatting

---
+++ Lab1Ex5/Program.Helpers.cs
@@ -0,0 +1,33 @@
+﻿partial class Program
+{
+    static int[] ReadNumbers(in uint NUMAR_ELEMENTE)
+    {
+        Console.WriteLine($"Introduceti cele {NUMAR_ELEMENTE} numere");
+        var numereRaw=Console.ReadLine()?.Split(" ");
+        if (numereRaw?.Length != NUMAR_ELEMENTE)
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            return ReadNumbers(NUMAR_ELEMENTE);
+        }
+        var numere= new int[NUMAR_ELEMENTE];
+        for(int i = 0; i < NUMAR_ELEMENTE; i++)
+        {
+            if (!int.TryParse(numereRaw?[i],out numere[i]))
+            {
+                Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+                return ReadNumbers(NUMAR_ELEMENTE);
+            }
+        }
+        return numere;
+    }
+    static int[] SortNumbers(int[] numere)
+    {
+        Array.Sort(numere);
+        return numere;
+    }
+    static void ShowNumbers(int[] numere)
+    {
+        foreach(var numar in numere)
+            Console.Write(numar+" ");
+    }
+}
